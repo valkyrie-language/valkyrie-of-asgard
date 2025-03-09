6054143ebe83b9d8eb77e036af245c0594274ddb
Aster <galaster@foxmail.com>
2025-03-09 14:48:19 +0800

    Use unified error handler

--- projects/docus-core/src/errors.rs
+++ projects/docus-core/src/errors.rs
@@ -1,6 +1,8 @@
-#[derive(Debug, Copy, Clone)]
-pub enum Error {
-    UnknownError
+#[derive(Debug, Clone)]
+pub enum DocusError {
+    RenderError { path: String, message: String },
+    EncodeError { format: String, message: String },
+    DecodeError { format: String, message: String },
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T> = std::result::Result<T, DocusError>;
