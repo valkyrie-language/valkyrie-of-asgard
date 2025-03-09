b8f467dcc5a8bde5fc31ebdb535bb03a86fbfa74
Aster <galaster@foxmail.com>
2025-03-09 14:51:59 +0800

    Replace renderer with comrak

--- projects/docus-core/src/render.rs
+++ projects/docus-core/src/render.rs
@@ -1,24 +1,38 @@
-use pulldown_cmark::{html, Options, Parser};
 use std::path::Path;
 use thiserror::Error;
+use askama::Template;
+use comrak::{markdown_to_html, ComrakOptions};
 
 #[derive(Debug, Error)]
 pub enum MarkdownError {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
+    #[error("Template error: {0}")]
+    Template(#[from] askama::Error),
+}
+
+#[derive(Template)]
+#[template(path = "page.html")]
+pub struct PageTemplate<'a> {
+    pub title: &'a str,
+    pub content: String,
+    pub sidebar: &'a str,
+    pub topbar: &'a str,
 }
 
 pub struct MarkdownRenderer {
-    options: Options,
+    options: ComrakOptions,
 }
 
 impl Default for MarkdownRenderer {
     fn default() -> Self {
-        let mut options = Options::empty();
-        options.insert(Options::ENABLE_TABLES);
-        options.insert(Options::ENABLE_FOOTNOTES);
-        options.insert(Options::ENABLE_STRIKETHROUGH);
-        options.insert(Options::ENABLE_TASKLISTS);
+        let mut options = ComrakOptions::default();
+        options.extension.table = true;
+        options.extension.strikethrough = true;
+        options.extension.tasklist = true;
+        options.extension.autolink = true;
+        options.extension.header_ids = Some(String::new());
+        options.render.github_pre_lang = true;
         Self { options }
     }
 }
@@ -28,15 +42,21 @@ impl MarkdownRenderer {
         Self::default()
     }
 
-    pub fn render_file(&self, path: impl AsRef<Path>) -> Result<String, MarkdownError> {
+    pub fn render_file(&self, path: impl AsRef<Path>, title: &str, sidebar: &str, topbar: &str) -> Result<String, MarkdownError> {
         let content = std::fs::read_to_string(path)?;
-        Ok(self.render_string(&content))
+        self.render_string(&content, title, sidebar, topbar)
     }
 
-    pub fn render_string(&self, content: &str) -> String {
-        let parser = Parser::new_ext(content, self.options);
-        let mut html_output = String::new();
-        html::push_html(&mut html_output, parser);
-        html_output
+    pub fn render_string(&self, content: &str, title: &str, sidebar: &str, topbar: &str) -> Result<String, MarkdownError> {
+        let html_output = markdown_to_html(content, &self.options);
+
+        let template = PageTemplate {
+            title,
+            content: html_output,
+            sidebar,
+            topbar,
+        };
+
+        Ok(template.render()?)
     }
 }
